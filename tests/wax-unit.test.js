bb8e2bc68f8461f52aba704df5d662317c7d47da
Aaron Caswell <aaron@captureplay.com>
2022-02-07 11:36:57 -0800

    ugh forgot to include the test suite

---
+++ tests/wax-unit.test.js
@@ -0,0 +1,62 @@
+const {
+  setupTestChain,
+  randomWamAccount,
+  createAccount,
+  setContract,
+  updateAuth,
+  linkauth,
+  eosjs,
+  transfer,
+  TESTING_PUBLIC_KEY,
+  getTableRows,
+  genericAction,
+} = require('../');
+
+describe('my test suite', () => {
+  beforeAll(async () => {
+    await setupTestChain();
+    await createAccount('mycontract11');
+
+    await setContract(
+      'mycontract11',
+      'tests/artifacts/testwax.wasm',
+      'tests/artifacts/testwax.abi'
+    );
+    await updateAuth('mycontract11', `active`, `owner`, {
+      threshold: 1,
+      accounts: [
+        {
+          permission: {
+            actor: 'mycontract11',
+            permission: `eosio.code`,
+          },
+          weight: 1,
+        },
+      ],
+      keys: [
+        {
+          key: TESTING_PUBLIC_KEY,
+          weight: 1,
+        },
+      ],
+      waits: [],
+    });
+
+    await transfer(
+      'eosio',
+      'mycontract11',
+      '200.00000000 WAX',
+      `sending some test funds`
+    );
+  });
+
+  it('my test case', async () => {
+    const balances = await getTableRows(
+      'eosio.token',
+      `accounts`,
+      'mycontract11'
+    );
+    expect(balances.length).toEqual(1);
+    expect(balances[0].balance).toEqual('200.00000000 WAX');
+  });
+});
